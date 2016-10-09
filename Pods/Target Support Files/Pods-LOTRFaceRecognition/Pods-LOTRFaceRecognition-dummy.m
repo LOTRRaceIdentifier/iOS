88080be5a510c556800a172e2db2e7a87b8e2bd5
Manas Purohit <manaspurohit@1x-vl943-72-19-95-157.wireless.umass.edu>
2016-10-09 00:08:52 -0400

    First

---
+++ Pods/Target Support Files/Pods-LOTRFaceRecognition/Pods-LOTRFaceRecognition-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LOTRFaceRecognition : NSObject
+@end
+@implementation PodsDummy_Pods_LOTRFaceRecognition
+@end
---
+++ Pods/Target Support Files/Pods-LOTRFaceRecognitionTests/Pods-LOTRFaceRecognitionTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LOTRFaceRecognitionTests : NSObject
+@end
+@implementation PodsDummy_Pods_LOTRFaceRecognitionTests
+@end
